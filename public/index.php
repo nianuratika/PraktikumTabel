9aebbb897e16e89340955a8d7d34a9973a15b1e7
ekaintansari <nianuratikasmile27@gmail.com>
2020-10-25 00:32:07 +0700

    Tabel

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<center>
+<h1>Tabel Mahasiswa</h1>
+<table border="1" cellpadding="10">
+<tr>
+        <td align=center>Nama</td>
+        <td align=center>NPM</td>
+		<td align=center>Semester</td>
+    </tr>
+    <tr>
+        <td align=center>Nia Nur Atika</td>
+        <td align=center>1817051015</td>
+		<td align=center>5</td>
+    </tr>
+</table>
+</center>
